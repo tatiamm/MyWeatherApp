cce346a3561fad5cc67707fe10f5ddbd33e45495
tatiamm <98728022+tatiamm@users.noreply.github.com>
2022-04-07 04:16:00 +0100

    update index forecast  (showing the next day instead of current)

--- src/final.js
+++ src/final.js
@@ -37,7 +37,7 @@ function displayForecast(response) {
 
   let forecastHTML = `<div class="row">`;
   forecast.forEach(function (forecastDay, index) {
-    if (index > 0 && index < 6) {
+    if (index > 0 && index < 7) {
       forecastHTML =
         forecastHTML +
         `
